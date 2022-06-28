3c80456132e9ee07ba614d9bf9d5ebbce51b582f
Bondarev Igor <igor.bondarev@mbicycle.com>
2022-06-28 14:43:34 +0300

    add style to header, create Home Page

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+//Config
+import { POSTER_SIZE, IMAGE_BASE_URL, BACKDROP_SIZE } from '../config';
+// Components
+
+// Hook
+
+// Image
+import NoImage from '../images/no_image.jpg';
+
+const Home = () => {
+
+
+
+  return <div>Home Page</div>
+}
+
