b72853be807c8b7336bc92abfffbc7890888a119
Bondarev Igor <igor.bondarev@mbicycle.com>
2022-06-28 17:46:53 +0300

    create useHomeFetch hook, add it to HomePage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 //API
 import API from '../API';
 //Config
@@ -6,38 +6,13 @@ import { POSTER_SIZE, IMAGE_BASE_URL, BACKDROP_SIZE } from '../config';
 // Components
 
 // Hook
-
+import { useHomeFetch } from '../Hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
 
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-
-      const movies = await API.fetchMovies(searchTerm, page);
-
-      setState(prev => ({
-        ...movies,
-        results:
-        page > 1 ? [...prev.results, movies.results] : [...movies.results] 
-      }));
-
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(false);
-  }
-
-  //Initial render
-  useEffect(() => {
-    fetchMovies(1)
-  }, []);
+  const { state, loading, error } = useHomeFetch();
 
   console.log(state);
 
