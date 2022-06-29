27b23dd4f3ad38522483d28508753e0540ce9b47
Bondarev Igor <igor.bondarev@mbicycle.com>
2022-06-29 18:21:26 +0300

    add searchterm logic

--- src/Hooks/useHomeFetch.js
+++ src/Hooks/useHomeFetch.js
@@ -10,7 +10,7 @@ const initialState = {
 }
 
 export const useHomeFetch = () => {
-  const [serchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -34,10 +34,11 @@ export const useHomeFetch = () => {
     setLoading(false);
   }
 
-  //Initial render
+  //Initial render and search
   useEffect(() => {
-    fetchMovies(1)
-  }, []);
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
-  return { state, loading, error, setSearchTerm };
+  return { state, loading, error, searchTerm, setSearchTerm };
 }
