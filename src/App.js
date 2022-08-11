fce610ba2828984eb985d0144d4b616a6bafeb99
Bondarev Igor <igor.bondarev@mbicycle.com>
2022-08-11 18:13:49 +0300

    create Movie NotFound components add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
+//Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //Components
 import Header from './components/Header';
 import Home from './components/Home.js';
+import Movie from './components/Movie.js';
+import NotFound from './components/NotFound.js';
+
 
 //Styles
 import { GlobalStyle } from './GlobalStyle';
 
 const  App = () => {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:movieId' element={<Movie />} />
+      </Routes>
       <GlobalStyle />
-    </div>
+    </Router>
   );
 }
 
