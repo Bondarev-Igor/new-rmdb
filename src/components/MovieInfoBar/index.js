a8511dbb967a6739f291e334926a8cdca1e934ce
Bondarev Igor <igor.bondarev@mbicycle.com>
2022-09-06 17:29:58 +0300

    start to develop MovieInfoBar component

---
+++ src/components/MovieInfoBar/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+//Helpers
+import { calcTime, convertMoney } from "../../helpers";
+//Styles
+import { Wrapper, Content } from "./MovieInfoBar.styles";
+
+const MovieInfoBar = ({ time, budget, revenue }) => (
+  <Wrapper>
+    <Content></Content>
+  </Wrapper>
+);
+
+export default MovieInfoBar;
