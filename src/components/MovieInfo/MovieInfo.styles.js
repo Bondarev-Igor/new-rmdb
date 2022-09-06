f079af25a07c019b5fdfefcc34d66c4de3816d15
Bondarev Igor <igor.bondarev@mbicycle.com>
2022-09-06 16:41:14 +0300

    add css styles to MovieInfo

--- src/components/MovieInfo/MovieInfo.styles.js
+++ src/components/MovieInfo/MovieInfo.styles.js
@@ -8,6 +8,7 @@ export const Wrapper = styled.div`
   background-position: center;
   padding: 40px 20px;
   animation: animateMovieInfo 1s;
+  max-height: 100%;
 
   @keyframes animateMovieInfo {
     from {
@@ -19,6 +20,54 @@ export const Wrapper = styled.div`
   }
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  background: rgba(0, 0, 0, 0.7);
+  border-radius: 20px;
 
-export const Text = styled.div``;
+  @media screen and (max-width: 768px) {
+    display: block;
+    max-height: none;
+  }
+`;
+
+export const Text = styled.div`
+  width: 100%;
+  padding: 20px 40px;
+  color: var(--white);
+  overflow: hidden;
+
+  .rating-directors {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .score {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    height: 40px;
+    background: #fff;
+    color: #000;
+    font-weight: 800;
+    border-radius: 50%;
+    margin: 0;
+  }
+
+  .director {
+    margin: 0 0 0 40px;
+
+    p {
+      margin: 0;
+    }
+  }
+
+  h1 {
+    @media screen and (max-width: 768px) {
+      font-size: var(--fontBig);
+    }
+  }
+`;
